e8e8516cb5e94d304d27ebdb47585bb008a6962c
winglovecola@gmail.com <winglovecola@gmail.com>
2023-03-26 03:48:06 -0400

    heroku deployment fix

--- server/server.js
+++ server/server.js
@@ -10,6 +10,15 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 app.use(routes);
 
 app.listen(PORT, () => {
