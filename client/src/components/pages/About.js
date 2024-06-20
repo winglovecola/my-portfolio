a2e28a1321497c4cecf67a09475319941df68676
winglovecola@gmail.com <winglovecola@gmail.com>
2024-06-20 18:37:41 -0400

    Updated about me

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -7,15 +7,25 @@ export default function About() {
 
       <div id="about-me-title" className="content-title">About Me</div>
       <div id="about-me-content" >
-        My name is Wing, and I have been working as a full-time web developer for more than 15 years. Therefore, I have developed excellent programming skills for web development in both front-end and back-end.
+      My name is Wing, and I bring over 20 years of experience as a dedicated web developer. My career has been marked by a strong proficiency in both front-end and back-end development, honed through a diverse range of projects and roles.
         <br/><br/>
-        Throughout most of my career, I worked with W&T Website Design, where I handled a wide variety of projects, including shopping sites, forums, chats, streaming sites, Flash game sites, and many others.
+        At W&T Website Design, I spearheaded numerous projects spanning e-commerce platforms, interactive forums, real-time chat applications, streaming services, and Flash game sites. This experience equipped me with comprehensive skills in web development.
         <br/><br/>
-        Later in my career path, I furthered my skills by working for TSquared Technology Solutions as a Full Stack Developer to develop complex applications. I handled tasks such as writing scripts for batch processing, utilizing Google API, and enhancing data security. I believe that the skills I learned in these positions would be a positive addition to your organization.
+        Transitioning to TSquared Technology Solutions as a Full Stack Developer, I furthered my expertise by tackling complex challenges such as scripting for batch processing, integrating Google APIs, and fortifying data security measures. These roles enriched my ability to deliver robust solutions tailored to business needs.
         <br/><br/>
-        In 2023, I decided to attend the University of Toronto to upgrade my programming skills by completing their Full Stack Developer boot camp program. During this program, I developed several projects using the latest programming languages and libraries.
+        In 2023, I pursued advanced training at the University of Toronto's Full Stack Developer boot camp, augmenting my proficiency with the latest programming languages and frameworks. This educational journey enabled me to execute several innovative projects, leveraging cutting-edge technologies.
         <br/><br/>
-        Throughout my career, I have developed a solid foundation and skills using the following libraries: JavaScript, Node, PHP, Python, React, Ionic, Angular, jQuery, CSS, Bootstrap, MySQL, MongoDB, Google API, Open AI API.
+        In 2024, I contributed as a Full Stack Developer at a startup, where I played a pivotal role in crafting web and native applications using React, React Native, Node.js, and various AWS services including Amazon Cognito, DynamoDB, AppSync, Lambda, Serverless, and Amplify.
+        <br/><br/>
+        My skill set encompasses a broad spectrum of technologies, including JavaScript, Node.js, PHP, Python, React, Ionic, Angular, jQuery, CSS, Bootstrap, MySQL, MongoDB, AWS Cloud, and integrations with Google API and OpenAI API.
+        <br/><br/>
+        I am eager to bring my extensive background and proactive approach to your esteemed organization, contributing to its ongoing success with innovative solutions and collaborative teamwork.
+        <br/><br/>
+        Thank you for considering my application. I look forward to the opportunity to discuss how my skills align with your needs.
+        <br/><br/>
+        Best regards,
+        <br/><br/>
+        Wing
 
       </div>
 
