de083712b7eea8a91db7cc77b0391978593d64ae
winglovecola@gmail.com <winglovecola@gmail.com>
2024-08-23 19:30:08 -0400

    Update profile description

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -7,17 +7,7 @@ export default function About() {
 
       <div id="about-me-title" className="content-title">About Me</div>
       <div id="about-me-content" >
-      My name is Wing, and I bring over 20 years of experience as a dedicated web developer. My career has been marked by a strong proficiency in both front-end and back-end development, honed through a diverse range of projects and roles.
-        <br/><br/>
-        At W&T Website Design, I spearheaded numerous projects spanning e-commerce platforms, interactive forums, real-time chat applications, streaming services, and Flash game sites. This experience equipped me with comprehensive skills in web development.
-        <br/><br/>
-        Transitioning to TSquared Technology Solutions as a Full Stack Developer, I furthered my expertise by tackling complex challenges such as scripting for batch processing, integrating Google APIs, and fortifying data security measures. These roles enriched my ability to deliver robust solutions tailored to business needs.
-        <br/><br/>
-        In 2023, I pursued advanced training at the University of Toronto's Full Stack Developer boot camp, augmenting my proficiency with the latest programming languages and frameworks. This educational journey enabled me to execute several innovative projects, leveraging cutting-edge technologies.
-        <br/><br/>
-        In 2024, I contributed as a Full Stack Developer at a startup, where I played a pivotal role in crafting web and native applications using React, React Native, Node.js, and various AWS services including Amazon Cognito, DynamoDB, AppSync, Lambda, Serverless, and Amplify.
-        <br/><br/>
-        My skill set encompasses a broad spectrum of technologies, including JavaScript, Node.js, PHP, Python, React, Ionic, Angular, jQuery, CSS, Bootstrap, MySQL, MongoDB, AWS Cloud, and integrations with Google API and OpenAI API.
+        Hello, my name is Wing, and I am a seasoned full-stack developer with over 15 years of experience in both front-end and back-end web development. I specialize in creating robust backend management tools and complex applications using technologies like React, Next.js, Tailwind CSS, Node.js, and a wide range of AWS services, including Amazon Cognito, DynamoDB, AppSync, Lambda, Amplify, S3, and the Serverless Framework. Additionally, I have expertise in integrating AI capabilities from ChatGPT, Google Gemini, or AWS to build powerful task automation solutions.
         <br/><br/>
         I am eager to bring my extensive background and proactive approach to your esteemed organization, contributing to its ongoing success with innovative solutions and collaborative teamwork.
         <br/><br/>
