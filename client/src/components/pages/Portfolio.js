1b45495b3d98e2941cee0adb907a05cc2c95625d
unknown <winglovecola@gmail.com>
2024-10-29 15:58:02 -0400

    updated work page

--- client/src/components/pages/Portfolio.js
+++ client/src/components/pages/Portfolio.js
@@ -21,7 +21,7 @@ export default function Home() {
               </div>
           </div>
 
-          <div id="workapp10" className="workapp-child">
+         {/*  <div id="workapp10" className="workapp-child">
               <div class="title">
                 <h2>Fate And Fortune</h2>
                 <span>React / AWS Cognito / DynamoDB / Lamda</span>
@@ -31,7 +31,7 @@ export default function Home() {
                   <div>App: <a href="https://fateandfortune.com/" target="_blank" rel="noreferrer">https://fateandfortune.com/</a></div>
                   <div>GitHub: Private</div>
               </div>
-          </div>
+          </div> */}
 
           <div id="workapp1" className="workapp-child">
               <div class="title">
